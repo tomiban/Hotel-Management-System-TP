508629ad506675fe991f3112cae12517f3b5b9ca
b4n <tbanchio15@gmail.com>
2024-06-12 03:48:18 -0300

    fix: Se agregan carpetas restantes

---
+++ Models/Usuario.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using MemoryPack;
+
+namespace GestionHotelWinForms.Models
+{
+    [MemoryPackable]
+    public partial class Usuario
+    {
+        
+        public int Id { get; set; }
+        [Required(ErrorMessage = "El nombre es requerido")]
+        public string Nombre { get; set; }
+        [Required(ErrorMessage = "El apellido es requerido")]
+        public string Apellido { get; set; }
+        [Required(ErrorMessage = "El nombre de usuario es requerido")]
+        public string Username { get; set; }
+        [Required(ErrorMessage = "La contraseña es requerida")]
+        public string Contraseña { get; set; }
+        [EnumDataType(typeof(Role), ErrorMessage = "El rol es requerido")]
+        public Role Role { get; set; }
+    }
+
+    public enum Role
+    {
+        Admin,
+        User
+    }
+}
---
+++ Services/ISerializeService.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GestionHotelWinForms.Services
+{
+    public interface ISerializeService
+    {
+        
+    }
+}
