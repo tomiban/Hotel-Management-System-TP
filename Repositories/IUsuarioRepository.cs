9ffbfba73c1b7468e558d2070672be939122b4c3
b4n <tbanchio15@gmail.com>
2024-07-25 15:02:24 -0300

    Mejora los repositorios

---
+++ Repositories/IUsuarioRepository.cs
@@ -0,0 +1,14 @@
+﻿using GestionHotelWinForms.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionHotelWinForms.Repositories
+{
+    public interface IUsuarioRepository: IRepository<Usuario>
+    {
+          Task<Usuario?> GetByUsernameAsync(string username);
+    }
+}
