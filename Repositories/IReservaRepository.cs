dcf8737069826220fe6b510bad40f21bd8811880
Tomib4n <tbanchio15@gmail.com>
2024-08-15 19:21:21 -0300

    crea e implementa interafaz y reserva repository

---
+++ Repositories/IReservaRepository.cs
@@ -0,0 +1,14 @@
+﻿using GestionHotelWinForms.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionHotelWinForms.Repositories
+{
+    public interface IReservaRepository : IRepository<Reserva>
+    {
+
+    }
+}
---
+++ Repositories/ReservaRepository.cs
@@ -0,0 +1,37 @@
+﻿using GestionHotelWinForms.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionHotelWinForms.Repositories
+{
+    public class ReservaRepository : IReservaRepository
+    {
+        public Task AddAsync(Reserva entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Reserva>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Reserva> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(Reserva entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
