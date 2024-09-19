8ce14b94f7be5028ff70e8d411c6dd670b9b2560
Tiziano Mendez <166566359+Tmendezz@users.noreply.github.com>
2024-09-19 20:43:36 -0300

    Botón Editar

--- src/Presentation/Program.cs
+++ src/Presentation/Program.cs
@@ -59,13 +59,13 @@ namespace Presentation
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
-           
-            IAdminPresenter adminPresenter = unityC.Resolve<AdminPresenter>();
+
+            ILoginPresenter loginPresenter = unityC.Resolve<LoginPresenter>();
             // Obtener la vista de login desde el presentador resuelto.
-            IAdminView adminView = adminPresenter.GetAdminView();
+            ILoginView loginView = loginPresenter.GetLoginView();
 
            
-            Application.Run((AdminView)adminView);
+            Application.Run((LoginView)loginView);
         }
     }
 }
