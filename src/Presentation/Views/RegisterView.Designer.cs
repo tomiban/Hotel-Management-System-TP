0d58f11d84c266b6a5d359ce8772236448264fa5
Tomib4n <tbanchio15@gmail.com>
2024-10-06 21:09:23 -0300

    fix errores de dependencias

--- src/Presentation/Views/RegisterView.Designer.cs
+++ src/Presentation/Views/RegisterView.Designer.cs
@@ -47,9 +47,11 @@
             // txtNombre
             // 
             txtNombre.AnimateReadOnly = false;
+            txtNombre.BackColor = Color.FromArgb(50, 50, 50);
             txtNombre.BorderStyle = BorderStyle.None;
             txtNombre.Depth = 0;
             txtNombre.Font = new Font("Roboto", 16F, FontStyle.Regular, GraphicsUnit.Pixel);
+            txtNombre.ForeColor = Color.FromArgb(222, 255, 255, 255);
             txtNombre.LeadingIcon = null;
             txtNombre.Location = new Point(122, 129);
             txtNombre.MaxLength = 32;
@@ -64,9 +66,11 @@
             // txtContraseña
             // 
             txtContraseña.AnimateReadOnly = false;
+            txtContraseña.BackColor = Color.FromArgb(50, 50, 50);
             txtContraseña.BorderStyle = BorderStyle.None;
             txtContraseña.Depth = 0;
             txtContraseña.Font = new Font("Roboto", 16F, FontStyle.Regular, GraphicsUnit.Pixel);
+            txtContraseña.ForeColor = Color.FromArgb(222, 255, 255, 255);
             txtContraseña.LeadingIcon = null;
             txtContraseña.Location = new Point(522, 322);
             txtContraseña.MaxLength = 16;
@@ -82,9 +86,11 @@
             // txtUsuario
             // 
             txtUsuario.AnimateReadOnly = false;
+            txtUsuario.BackColor = Color.FromArgb(50, 50, 50);
             txtUsuario.BorderStyle = BorderStyle.None;
             txtUsuario.Depth = 0;
             txtUsuario.Font = new Font("Roboto", 16F, FontStyle.Regular, GraphicsUnit.Pixel);
+            txtUsuario.ForeColor = Color.FromArgb(222, 255, 255, 255);
             txtUsuario.LeadingIcon = null;
             txtUsuario.Location = new Point(122, 322);
             txtUsuario.MaxLength = 32;
@@ -99,9 +105,11 @@
             // txtTelefono
             // 
             txtTelefono.AnimateReadOnly = false;
+            txtTelefono.BackColor = Color.FromArgb(50, 50, 50);
             txtTelefono.BorderStyle = BorderStyle.None;
             txtTelefono.Depth = 0;
             txtTelefono.Font = new Font("Roboto", 16F, FontStyle.Regular, GraphicsUnit.Pixel);
+            txtTelefono.ForeColor = Color.FromArgb(222, 255, 255, 255);
             txtTelefono.LeadingIcon = null;
             txtTelefono.Location = new Point(522, 225);
             txtTelefono.MaxLength = 15;
@@ -116,9 +124,11 @@
             // txtEdad
             // 
             txtEdad.AnimateReadOnly = false;
+            txtEdad.BackColor = Color.FromArgb(50, 50, 50);
             txtEdad.BorderStyle = BorderStyle.None;
             txtEdad.Depth = 0;
             txtEdad.Font = new Font("Roboto", 16F, FontStyle.Regular, GraphicsUnit.Pixel);
+            txtEdad.ForeColor = Color.FromArgb(222, 255, 255, 255);
             txtEdad.LeadingIcon = null;
             txtEdad.Location = new Point(122, 225);
             txtEdad.MaxLength = 3;
@@ -133,9 +143,11 @@
             // txtApellido
             // 
             txtApellido.AnimateReadOnly = false;
+            txtApellido.BackColor = Color.FromArgb(50, 50, 50);
             txtApellido.BorderStyle = BorderStyle.None;
             txtApellido.Depth = 0;
             txtApellido.Font = new Font("Roboto", 16F, FontStyle.Regular, GraphicsUnit.Pixel);
+            txtApellido.ForeColor = Color.FromArgb(222, 255, 255, 255);
             txtApellido.LeadingIcon = null;
             txtApellido.Location = new Point(522, 129);
             txtApellido.MaxLength = 32;
@@ -150,8 +162,10 @@
             // lblNombre
             // 
             lblNombre.AutoSize = true;
+            lblNombre.BackColor = Color.FromArgb(50, 50, 50);
             lblNombre.Depth = 0;
             lblNombre.Font = new Font("Roboto", 14F, FontStyle.Regular, GraphicsUnit.Pixel);
+            lblNombre.ForeColor = Color.FromArgb(222, 255, 255, 255);
             lblNombre.Location = new Point(122, 107);
             lblNombre.MouseState = MaterialSkin.MouseState.HOVER;
             lblNombre.Name = "lblNombre";
@@ -162,8 +176,10 @@
             // lblApellido
             // 
             lblApellido.AutoSize = true;
+            lblApellido.BackColor = Color.FromArgb(50, 50, 50);
             lblApellido.Depth = 0;
             lblApellido.Font = new Font("Roboto", 14F, FontStyle.Regular, GraphicsUnit.Pixel);
+            lblApellido.ForeColor = Color.FromArgb(222, 255, 255, 255);
             lblApellido.Location = new Point(522, 107);
             lblApellido.MouseState = MaterialSkin.MouseState.HOVER;
             lblApellido.Name = "lblApellido";
@@ -174,8 +190,10 @@
             // lblEdad
             // 
             lblEdad.AutoSize = true;
+            lblEdad.BackColor = Color.FromArgb(50, 50, 50);
             lblEdad.Depth = 0;
             lblEdad.Font = new Font("Roboto", 14F, FontStyle.Regular, GraphicsUnit.Pixel);
+            lblEdad.ForeColor = Color.FromArgb(222, 255, 255, 255);
             lblEdad.Location = new Point(122, 203);
             lblEdad.MouseState = MaterialSkin.MouseState.HOVER;
             lblEdad.Name = "lblEdad";
@@ -186,8 +204,10 @@
             // lblTelefono
             // 
             lblTelefono.AutoSize = true;
+            lblTelefono.BackColor = Color.FromArgb(50, 50, 50);
             lblTelefono.Depth = 0;
             lblTelefono.Font = new Font("Roboto", 14F, FontStyle.Regular, GraphicsUnit.Pixel);
+            lblTelefono.ForeColor = Color.FromArgb(222, 255, 255, 255);
             lblTelefono.Location = new Point(522, 203);
             lblTelefono.MouseState = MaterialSkin.MouseState.HOVER;
             lblTelefono.Name = "lblTelefono";
@@ -198,8 +218,10 @@
             // lblUsuario
             // 
             lblUsuario.AutoSize = true;
+            lblUsuario.BackColor = Color.FromArgb(50, 50, 50);
             lblUsuario.Depth = 0;
             lblUsuario.Font = new Font("Roboto", 14F, FontStyle.Regular, GraphicsUnit.Pixel);
+            lblUsuario.ForeColor = Color.FromArgb(222, 255, 255, 255);
             lblUsuario.Location = new Point(122, 300);
             lblUsuario.MouseState = MaterialSkin.MouseState.HOVER;
             lblUsuario.Name = "lblUsuario";
@@ -211,10 +233,12 @@
             // 
             btnRegistrarse.AutoSize = false;
             btnRegistrarse.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            btnRegistrarse.BackColor = Color.FromArgb(50, 50, 50);
             btnRegistrarse.Cursor = Cursors.Hand;
             btnRegistrarse.Density = MaterialSkin.Controls.MaterialButton.MaterialButtonDensity.Default;
             btnRegistrarse.Depth = 0;
             btnRegistrarse.Enabled = false;
+            btnRegistrarse.ForeColor = Color.FromArgb(222, 255, 255, 255);
             btnRegistrarse.HighEmphasis = true;
             btnRegistrarse.Icon = null;
             btnRegistrarse.Location = new Point(353, 415);
@@ -227,13 +251,15 @@
             btnRegistrarse.Text = "Registrarse";
             btnRegistrarse.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Contained;
             btnRegistrarse.UseAccentColor = false;
-            btnRegistrarse.UseVisualStyleBackColor = true;
+            btnRegistrarse.UseVisualStyleBackColor = false;
             // 
             // lblContraseña
             // 
             lblContraseña.AutoSize = true;
+            lblContraseña.BackColor = Color.FromArgb(50, 50, 50);
             lblContraseña.Depth = 0;
             lblContraseña.Font = new Font("Roboto", 14F, FontStyle.Regular, GraphicsUnit.Pixel);
+            lblContraseña.ForeColor = Color.FromArgb(222, 255, 255, 255);
             lblContraseña.Location = new Point(522, 300);
             lblContraseña.MouseState = MaterialSkin.MouseState.HOVER;
             lblContraseña.Name = "lblContraseña";
@@ -244,14 +270,14 @@
             // btnRegresar
             // 
             btnRegresar.AutoSizeMode = AutoSizeMode.GrowAndShrink;
-            btnRegresar.BackColor = Color.FromArgb(64, 64, 64);
+            btnRegresar.BackColor = Color.FromArgb(50, 50, 50);
             btnRegresar.Cursor = Cursors.Hand;
             btnRegresar.Density = MaterialSkin.Controls.MaterialButton.MaterialButtonDensity.Default;
             btnRegresar.Depth = 0;
             btnRegresar.FlatAppearance.BorderColor = Color.Gray;
             btnRegresar.FlatAppearance.MouseDownBackColor = Color.Gray;
             btnRegresar.FlatAppearance.MouseOverBackColor = Color.Gray;
-            btnRegresar.ForeColor = SystemColors.ControlDarkDark;
+            btnRegresar.ForeColor = Color.FromArgb(222, 255, 255, 255);
             btnRegresar.HighEmphasis = true;
             btnRegresar.Icon = null;
             btnRegresar.Location = new Point(407, 467);
--- src/Services/Services/AuthService.cs
+++ src/Services/Services/AuthService.cs
@@ -5,7 +5,6 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
-using BCrypt.Net;
 using InfraestructureLayer.Helpers;
 
 namespace ApplicationLayer.Services
